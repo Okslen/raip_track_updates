a3ac4ca2e4e6c52439d46f24d5372cae00600721
Игорь Дроздовский <okslen@gmail.com>
2024-05-07 12:44:51 +0300

    change get_response sleep time

--- tracker/utils.py
+++ tracker/utils.py
@@ -1,24 +1,28 @@
 import logging
+from time import sleep
 from typing import Optional, Union
 
 from bs4 import BeautifulSoup, Tag
 from requests import RequestException, Response
 from requests_html import HTMLSession
 
-from settings import ENCODING
+from settings import DELAY, ENCODING
 from exceptions import ParserFindTagException
 
 
-def get_response(url: str, sleep: Optional[int]) -> Union[Response, None]:
+def get_response(url: str, sleep_time: Optional[int]) -> Union[Response, None]:
     session = HTMLSession()
     try:
-        response = session.get(url)
+        response: Response = session.get(url)
     except RequestException:
         # logging.error(f'Возникла ошибка при загрузке {url}',
         #              exc_info=True, stack_info=True)
+        logging.error(f'Неудачный запрос, попробую секунд через {DELAY}')
+        sleep(DELAY)
+        get_response(url, sleep_time)
         return None
-    if sleep:
-        response.html.render(sleep=sleep)
+    if sleep_time:
+        response.html.render(sleep=sleep_time)
     return response
 
 
